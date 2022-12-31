145ced5eea636537eebc69c600af7640c2def7da
Ritika Shaw <ritikashaw306@gmail.com>
2022-12-31 12:04:17 +0530

    circle check code added

---
+++ for_loop/circle_check.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int x , y,r, x1,y1;
+    printf ("Enter the center x cordinate");
+    scanf("%d",&x);
+    printf ("Enter the center y cordinate");
+    scanf("%d",&y);
+    printf ("Enter the radius");
+    scanf("%d",&r);
+    printf ("Enter the point x cordinate");
+    scanf("%d",&x1);
+    printf ("Enter the point y cordinate");
+    scanf("%d",&y1);
+    int d = sqrt(pow((x-x1),2)+pow((y-y1),2));
+    if(d<r)
+    {
+        printf("The point lies within the circle");
+    }
+    else if(d>r)
+    {
+      printf("The point lies outside the circle");  
+    }
+    else(d==r){
+        printf("The point lies on the circle");
+    }
+    return 0;
+    
+}
